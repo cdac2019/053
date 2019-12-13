0c01fd0a545071615f82317886df794759c59164
cdac@182 <cdac2019>
2019-12-13 18:31:08 +0530

    hi

---
+++ src/inheritance/base.java
@@ -0,0 +1,31 @@
+package inheritance;
+
+class parent
+{
+   int b ;
+   char c ;
+   void setx(int b1)
+   {
+	 System.out.println("i am the base class of set method one");  
+   }
+}
+   class child extends parent
+   {
+	   void setx(int c1) 
+	   {
+		   
+	 System.out.println("i am the child class of set method");     
+	   }
+   }
+   
+public class base
+{
+	public static void main(String[] args)
+	{
+//     base ba = new base ();
+//     ba.setx(20);
+     child ch = new child();
+     ch.setx(10);
+	}
+
+}
